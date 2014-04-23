5c62fb144fee9d6beccbc594b15452f0f0ca5375
meonwax <meonwax@gmail.com>
2014-04-23 13:52:49 +0200

    Moved gallery to own PHP class

--- classes/SmartySingleton.php
+++ classes/SmartySingleton.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('libs/Smarty.class.php');
+require_once('../smarty/libs/Smarty.class.php');
 
 class SmartySingleton {
 
@@ -28,10 +28,9 @@ class SmartySingleton {
 
 			self::$instance = $smarty;
 		};
+
 		return self::$instance;
 	}
 }
-
-$smarty = SmartySingleton::instance();
 
 ?>
