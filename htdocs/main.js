25b42b81463fcce859a5bcae5eb4717ffba3b9bb
meonwax <meonwax@gmail.com>
2014-05-18 23:15:06 +0200

    Create all flickr-like thumbnail sizes

--- htdocs/main.js
+++ htdocs/main.js
@@ -1,11 +1,4 @@
 $("#gallery").justifiedGallery( {
-	sizeRangeSuffixes: {
-		'lt100':'',
-		'lt240':'',
-		'lt320':'',
-		'lt500':'',
-		'lt640':'',
-		'lt1024':'' },
 	justifyLastRow : false,
 	margins : 2,
 	captions : false
